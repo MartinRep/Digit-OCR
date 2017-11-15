4bb53ed118aa2b4b7bd5956daa69b77788a525c0
MartinRep <repickym@gmail.com>
2017-11-15 11:51:27 +0000

    Upload file done

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+# https://stackoverflow.com/questions/28982974/flask-restful-upload-image#28983544
 from flask import Flask, jsonify, render_template, request
+import os
 app = Flask(__name__)
  
 @app.route("/")
-def hello():
-    return render_template('index.html', name='martin')
+def getImage():
+    return render_template('index.html')
+
+@app.route("/uploadimage", methods=['GET','POST'])
+def processImage():
+    file = request.files['myfile']
+    extension = os.path.splitext(file.filename)[1]
+    file.save('uploads/'+file.filename)
+    return ('image uploaded: '+file.filename)
  
 if __name__ == "__main__":
     app.run()
